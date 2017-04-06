60218469ea5c79e1826e1a868d1f14f1f0eb5b3a
k241285@mail.ru <aleksandr.chasovnikov@gmail.com>
2017-04-06 15:40:27 +0300

    my-snippets

--- js/my.js
+++ js/my.js
@@ -1,21 +1,24 @@
-      $(document).ready(function() {
+$(document).ready(function() {
 
-        $(window).scroll(function() {
-        // Высота проявления кнопки
-        if ($(this).scrollTop() > 100) {
-          $('#go-to-top').fadeIn();
-        } else {
-          $('#go-to-top').fadeOut();
-        }
-      });
+  $(window).scroll(function() {  // Высота проявления кнопки
+    if ($(this).scrollTop() > 100) {
+      $('#go-to-top').fadeIn();
+    } else {
+      $('#go-to-top').fadeOut();
+    }
+  });
 
-        $('#go-to-top').click(function() {
-          $('body,html').animate({
-            scrollTop: 0
-        // Скорость подъема
-      }, 2000);
-          return false;
-        });
+  $('#go-to-top').click(function() {
+    $('body,html').animate({
+      scrollTop: 0  // Скорость подъема
+    }, 2000);
+    return false;
+  });
 
-      });
+  $('.button-show').click(function() {
+    $('.col-xs-3').css('left', '0').css('position','relative');
+    $('.col-xs-3').toggle( "slow" );
+  });
+
+});
 
