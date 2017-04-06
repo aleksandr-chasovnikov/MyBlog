e3d6f77bdad97c23abee27db4e8aaaa77a8cc8e3
k241285@mail.ru <aleksandr.chasovnikov@gmail.com>
2017-04-06 20:15:02 +0300

    next

--- js/my.js
+++ js/my.js
@@ -1,24 +1,20 @@
 $(document).ready(function() {
 
-  $(window).scroll(function() {  // Высота проявления кнопки
-    if ($(this).scrollTop() > 100) {
-      $('#go-to-top').fadeIn();
-    } else {
-      $('#go-to-top').fadeOut();
-    }
-  });
-
-  $('#go-to-top').click(function() {
-    $('body,html').animate({
-      scrollTop: 0  // Скорость подъема
-    }, 2000);
-    return false;
-  });
-
-  $('.button-show').click(function() {
-    $('.col-xs-3').css('left', '0').css('position','relative');
-    $('.col-xs-3').toggle( "slow" );
-  });
+    $(window).scroll(function() {
+        // Высота проявления кнопки
+        if ($(this).scrollTop() > 200) {
+            $('.go-to-top').fadeIn();
+        } else {
+            $('.go-to-top').fadeOut();
+        }
+    });
+    
+    $('.go-to-top').click(function() {
+        $('body,html').animate({
+            scrollTop: 0
+        // Скорость подъема
+        }, 1000);
+        return false;
+    });
 
 });
-
