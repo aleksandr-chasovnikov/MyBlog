a8da486ca6799d8ad67196850e087047f8e0af8a
k241285@mail.ru <aleksandr.chasovnikov@gmail.com>
2017-04-23 10:25:14 +0300

    (autoload) scandir(ROOT)

--- components/Autoload.php
+++ components/Autoload.php
@@ -6,17 +6,13 @@
 function __autoload($class_name)
 {
     // Массив папок, в которых могут находиться необходимые классы
-    $array_paths = array(
-        '/models/',
-        '/components/',
-        '/controllers/',
-    );
+    $array_paths = scandir(ROOT);
 
     // Проходим по массиву папок
     foreach ($array_paths as $path) {
 
         // Формируем имя и путь к файлу с классом
-        $path = ROOT . $path . $class_name . '.php';
+        $path = ROOT . "/" . $path . "/" . $class_name . '.php';
 
         // Если такой файл существует, подключаем его
         if (is_file($path)) {
